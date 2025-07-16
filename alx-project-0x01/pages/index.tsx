8207ec007c8cf8d37d4862e1c0e27bc52f83da21
KOKETSO PHIRI <120306832+KoketsoPhiri@users.noreply.github.com>
2025-07-16 15:40:03 +0200

    header

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '@components/layout/Header';
 
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Welcome Page</h1>
+      <h1 className="text-7xl font-thin"> Welcome to our Application!</h1>
+      <Header />
     </div>
   )
 }
